2dc8f652d1113842513f1edfa81cd03ed3a84893
Albob50a <51758428+Albob50a@users.noreply.github.com>
2019-07-03 17:23:51 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Eris = require("eris");
+var x5bzteam = new Eris(process.env.BOT_TOKEN);
+x5bzteam.on("ready", () => {
+    console.log("Yo! Changer [ON]");
+    onstart();
+});
+x5bz = "537966671810527235";
+nick = "!.💛DrTriCK💛.👑";
+
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){
+     
+     if (i == v1.length){
+     clearInterval(x5bz3);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);
+ 
+        i++;
+   
+     }, 500);
+}
+x5bzteam.connect();
